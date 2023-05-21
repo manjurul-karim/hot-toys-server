0eeaae72b32118f69a8b207dbc5f8eff7049f347
manjurul17002 <manjurul17002@gmail.com>
2023-05-21 23:18:31 +0600

    index change

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
 
     const carCollection = client.db("carDB").collection("allCars");
     const toysCollection = client.db("carDB").collection("addedToys");
