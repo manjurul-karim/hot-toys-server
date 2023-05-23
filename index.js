4565f4bf1eb8549bd6ad07314e9dc90cd0c7fdc8
manjurul17002 <manjurul17002@gmail.com>
2023-05-23 22:58:36 +0600

    new commit

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/cardetails/:id", async (req, res) => {
+    app.get("/allcars/:id", async (req, res) => {
       const id = req.params.id;
       console.log(id);
       const query = { _id: new ObjectId(id) };
